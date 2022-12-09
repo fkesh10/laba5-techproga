95909cbfccdc1bbe88a0ed19b338b3c08d0d0290
DrukardKirA <fkesh10@gmail.com>
2022-12-09 19:25:33 +0300

    works done laba5 complete

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,6 +1,7 @@
 from articles.models import Article
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import redirect
 
 def archive(request):
     return render(request, 'archive.html', {"posts": Article.objects.all()})
@@ -10,6 +11,34 @@ def get_article(request, article_id):
         post = Article.objects.get(id=article_id)
         return render(request, 'article.html', {"post": post})
     except Article.DoesNotExist:
+        raise Http404
+
+def create_post(request):
+    if not request.user.is_anonymous:
+        if request.method == "POST":
+        # обработать данные формы, если метод POST
+            form = {
+                'text': request.POST["text"], 'title': request.POST["title"]
+            }
+        # в словаре form будет храниться информация, введенная пользователем
+            if form["text"] and form["title"]:
+        # если поля заполнены без ошибок проверяем уникальна ли статья
+                if_article_unique = Article.objects.filter(title=form["title"])
+                if len(if_article_unique) == 0:
+                    article = Article.objects.create(text=form["text"], title=form["title"], author=request.user)
+                    return redirect('get_article', article_id=article.id)
+                else:
+                    form['errors'] = u"Такая статья уже существует"
+                    return render(request, 'form.html', {'form': form})
+            # перейти на страницу поста
+            else:
+        # если введенные данные некорректны
+                form['errors'] = u"Не все поля заполнены"
+                return render(request, 'form.html', {'form': form})
+        else:
+        # просто вернуть страницу с формой, если метод GET
+            return render(request, 'form.html', {})
+    else:
         raise Http404
 
 # def archive(request):
